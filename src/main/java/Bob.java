9d935fc31f288685d3c3bcac6d8a47f70275ae59
TYS2 <yweesee@gmail.com>
2026-08-19 20:32:47 +0200

    rename, greet and exit

---
+++ src/main/java/Bob.java
@@ -0,0 +1,8 @@
+public class Bob {
+    public static void main(String[] args) {
+        String message = "Hello! I'm Bob.\n"
+                        + "What can I do for you? \n"
+                        + "Bye. Hope to see you again soon!";
+        System.out.println(message);
+    }
+}
