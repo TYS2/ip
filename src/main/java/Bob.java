b2627c701076b6aa714ee99bb89a3e274628f9c9
TYS2 <yweesee@gmail.com>
2026-08-19 21:47:12 +0200

    todos, events, deadlines

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -51,13 +51,59 @@ public class Bob {
                 continue;
             }
 
-            Task task = new Task(command);
-            tasks.add(task);
+            if (command.startsWith("todo ")) {
+                String description = command.substring(5);
 
-            System.out.println("added: " + task);
+                Task task = new Task(description);
+                tasks.add(task);
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + task);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+
+                continue;
+            }
+
+            if (command.startsWith("deadline ")) {
+                String input = command.substring(9);
+
+                String[] parts = input.split(" /by ", 2);
+
+                String description = parts[0];
+                String end = parts[1];
+
+                Task task = new Deadline(description, end);
+                tasks.add(task);
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + task);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+
+                continue;
+            }
+
+            if (command.startsWith("event ")) {
+                String input = command.substring(6);
+
+                String[] parts = input.split(" /from | /to ", 3);
+
+                String description = parts[0];
+                String from = parts[1];
+                String to = parts[2];
+
+                Task task = new Event(description, from, to);
+                tasks.add(task);
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + task);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+
+                continue;
+            }
+
+            System.out.println("I don't understand that command.");
         }
 
         scanner.close();
     }
-
 }
