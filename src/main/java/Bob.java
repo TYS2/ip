0d8e11b2d36ee6f868b8611e80a02f5b3bb96ea2
TYS2 <yweesee@gmail.com>
2026-08-19 20:45:33 +0200

    echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String message = "Hello! I'm Bob.\n"
-                        + "What can I do for you? \n"
-                        + "Bye. Hope to see you again soon!";
+                + "What can I do for you? \n"
+                + "Bye. Hope to see you again soon!";
         System.out.println(message);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(command);
+        }
+
+        scanner.close();
     }
 }
