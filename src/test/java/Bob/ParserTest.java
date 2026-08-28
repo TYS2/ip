5e88696d25168a240f3851c36bfecbfe8603426d
Teng Ywee See <yweesee@gmail.com>
2026-08-28 20:54:18 +0800

    Add test cases for Parser and TaskList

---
+++ src/test/java/Bob/ParserTest.java
@@ -0,0 +1,35 @@
+package Bob;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @DisplayName("Test for parsing command")
+    @Test
+    public void testCheckForCommand(){
+        Parser parser=new Parser();
+        assertEquals(CommandType.BYE, parser.parse("bye"));
+    }
+
+    @DisplayName("Test for parsing the list command")
+    @Test
+    public void testCheckForListCommand() {
+        Parser parser = new Parser();
+        assertEquals(CommandType.LIST, parser.parse("list"));
+    }
+
+    @DisplayName("Test for creating a deadline task")
+    @Test
+    public void testAddDeadlineTask() throws BobException {
+        TaskList taskList = new TaskList();
+
+        Task deadline = taskList.addDeadline("Submit report /by 2026-09-01");
+
+        assertEquals(1, taskList.size());
+        assertEquals(
+                "[D][ ] Submit report (by: Sep 01 2026)",
+                deadline.toString());
+    }
+}
---
+++ src/test/java/Bob/TaskListTest.java
@@ -0,0 +1,54 @@
+package Bob;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TaskListTest {
+    @Test
+    @DisplayName("Adding a todo increases the task count and stores its description")
+    public void addTodo_validDescription_taskIsAdded() throws BobException {
+        TaskList taskList = new TaskList();
+
+        Task task = taskList.addTodo("Read a book");
+
+        assertEquals(1, taskList.size());
+        assertEquals("Read a book", task.getItem());
+        assertEquals(task, taskList.get(0));
+    }
+
+    @Test
+    @DisplayName("Adding an empty todo throws an exception")
+    public void addTodo_emptyDescription_exceptionThrown() {
+        TaskList taskList = new TaskList();
+
+        assertThrows(BobException.class, () -> taskList.addTodo(""));
+        assertEquals(0, taskList.size());
+    }
+
+    @Test
+    @DisplayName("Deleting a task removes the requested one")
+    public void deleteTask_validTaskNumber_taskRemoved() throws BobException {
+        TaskList taskList = new TaskList();
+        taskList.addTodo("First task");
+        taskList.addTodo("Second task");
+
+        Task deleted = taskList.deleteTask(1);
+
+        assertEquals("First task", deleted.getItem());
+        assertEquals(1, taskList.size());
+        assertEquals("Second task", taskList.get(0).getItem());
+    }
+
+    @Test
+    @DisplayName("Deleting an invalid task number throws an exception")
+    public void deleteTask_invalidTaskNumber_exceptionThrown() throws BobException {
+        TaskList taskList = new TaskList();
+        taskList.addTodo("Only task");
+
+        assertThrows(BobException.class, () -> taskList.deleteTask(2));
+        assertEquals(1, taskList.size());
+    }
+}
